80f912fd5f09e7d36cedf2cfb4f65036501d5dc4
Campos Ferreira <lucascampos_ferreira.ext@siemens.com>
2021-01-26 15:21:40 -0300

    Aula 1 + Desafios

--- pages/index.js
+++ pages/index.js
@@ -20,29 +20,28 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <IndexPage />
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-        </Widget>
+      <QuizBackground backgroundImage={db.bg}>
+        <IndexPage />
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>{db.description}</p>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
-
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/LCF1994" />
-    </QuizBackground>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/LCF1994" />
+      </QuizBackground>
   );
 }
--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -3,12 +3,10 @@ import db from '../../../db.json'
 
 function IndexPage() {
   return (
-    <div>
       <Head>
         <title>{db.title}</title>
         <meta property="og:image" content={db.bg} key={db.title} />
       </Head>
-    </div>
   )
 }
 
